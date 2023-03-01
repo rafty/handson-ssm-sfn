43a89b38805e31a2786055e68747e370e9b5f8d2
takashi <yagita.takashi@gmail.com>
2023-03-01 14:16:21 +0900

    first commit

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ app = cdk.App()
 
 vpc_stack.HandsonVpc(app, 'HandsonVpcStack', env=env)
 
-run_command_stack.RunCommandStack(app, "RunCommandStack", env=env)
+# run_command_stack.RunCommandStack(app, "RunCommandStack", env=env)
 
-automation_stack.AutomationRunBookStack(app, 'AutomationRunBookStack', env=env)
+# automation_stack.AutomationRunBookStack(app, 'AutomationRunBookStack', env=env)
 
-stepfunctions_stack.StepfunctionsStack(app, 'StepfunctionsStack', env=env)
+# stepfunctions_stack.StepfunctionsStack(app, 'StepfunctionsStack', env=env)
 
 
 app.synth()
